892062d92ba2a66a6d3ffe92e1ed59a6c62d9976
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2024-05-20 20:51:09 +0400

    Add IEvent interface

--- Event.cs
+++ Event.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 
 namespace GeneralUtils {
-    public class Event {
+    public class Event : IEvent {
         private Action _event;
-
-        public Event(out Action invoker) {
-            invoker = () => _event?.Invoke();
-        }
 
         public void Subscribe(Action subscriber) {
             _event += subscriber;
@@ -19,14 +15,20 @@ namespace GeneralUtils {
         public void ClearSubscribers() {
             _event = null;
         }
+
+        public void Invoke() {
+            _event?.Invoke();
+        }
     }
 
-    public class Event<T> {
-        private Action<T> _event;
+    public interface IEvent {
+        public void Subscribe(Action subscriber);
+        public void Unsubscribe(Action subscriber);
+        public void ClearSubscribers();
+    }
 
-        public Event(out Action<T> invoker) {
-            invoker = value => _event?.Invoke(value);
-        }
+    public class Event<T> : IEvent<T> {
+        private Action<T> _event;
 
         public void Subscribe(Action<T> subscriber) {
             _event += subscriber;
@@ -39,14 +41,20 @@ namespace GeneralUtils {
         public void ClearSubscribers() {
             _event = null;
         }
+
+        public void Invoke(T value) {
+            _event?.Invoke(value);
+        }
     }
 
-    public class Event<T1, T2> {
-        private Action<T1, T2> _event;
+    public interface IEvent<out T> {
+        public void Subscribe(Action<T> subscriber);
+        public void Unsubscribe(Action<T> subscriber);
+        public void ClearSubscribers();
+    }
 
-        public Event(out Action<T1, T2> invoker) {
-            invoker = (v1, v2) => _event?.Invoke(v1, v2);
-        }
+    public class Event<T1, T2> : IEvent<T1, T2> {
+        private Action<T1, T2> _event;
 
         public void Subscribe(Action<T1, T2> subscriber) {
             _event += subscriber;
@@ -59,14 +67,20 @@ namespace GeneralUtils {
         public void ClearSubscribers() {
             _event = null;
         }
+
+        public void Invoke(T1 value1, T2 value2) {
+            _event?.Invoke(value1, value2);
+        }
     }
 
-    public class Event<T1, T2, T3> {
-        private Action<T1, T2, T3> _event;
+    public interface IEvent<out T1, out T2> {
+        public void Subscribe(Action<T1, T2> subscriber);
+        public void Unsubscribe(Action<T1, T2> subscriber);
+        public void ClearSubscribers();
+    }
 
-        public Event(out Action<T1, T2, T3> invoker) {
-            invoker = (v1, v2, v3) => _event?.Invoke(v1, v2, v3);
-        }
+    public class Event<T1, T2, T3> : IEvent<T1, T2, T3> {
+        private Action<T1, T2, T3> _event;
 
         public void Subscribe(Action<T1, T2, T3> subscriber) {
             _event += subscriber;
@@ -79,5 +93,15 @@ namespace GeneralUtils {
         public void ClearSubscribers() {
             _event = null;
         }
+
+        public void Invoke(T1 value1, T2 value2, T3 value3) {
+            _event?.Invoke(value1, value2, value3);
+        }
+    }
+
+    public interface IEvent<out T1, out T2, out T3> {
+        public void Subscribe(Action<T1, T2, T3> subscriber);
+        public void Unsubscribe(Action<T1, T2, T3> subscriber);
+        public void ClearSubscribers();
     }
 }
